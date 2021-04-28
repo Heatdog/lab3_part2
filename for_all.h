dd3d8f69937adcb1492c2225fed19b0075c3e277
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-04-28 12:55:58 +0300

    Create for_all.h

---
+++ for_all.h
@@ -0,0 +1,22 @@
+//
+// Created by yura on 26.04.2021.
+//
+
+#ifndef LAB3_PART2_FOR_ALL_H
+#define LAB3_PART2_FOR_ALL_H
+
+#include "hash.h"
+
+
+char *scan_string(char *);
+void scan_int(int*);
+Info *scan_info(Info*);
+void dialog(Table* table);
+void D_Add(Table*);
+void D_Find(Table*);
+void D_Delete(Table*);
+void D_Show(Table*);
+
+
+
+#endif //LAB3_PART2_FOR_ALL_H
